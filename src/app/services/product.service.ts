27110e1d8b9f2d8d3db27b225f66f9e7142c7c4c
vikz95 <viktor.zahariev@protonmail.com>
2020-06-19 12:17:47 +0200

    Categories menu now loads the categories dynamically from the DB.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,10 +3,17 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Product} from '../common/product';
 import {map} from 'rxjs/operators';
+import {ProductCategory} from '../common/product-category';
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
 
@@ -16,13 +23,19 @@ interface GetResponse {
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl)
+    return this.httpClient.get<GetResponseProducts>(searchUrl)
       .pipe(map(response => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map(response => response._embedded.productCategory));
   }
 }
