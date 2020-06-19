9daffa0d2471f18b56d9c3a74407ab9c752127cb
vikz95 <viktor.zahariev@protonmail.com>
2020-06-19 11:00:59 +0200

    Sobstituted app-product-list with a router-outlet as the main content of the page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ProductService} from './services/product.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
@@ -12,10 +21,12 @@ import {ProductService} from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot((routes)),
     BrowserModule,
     HttpClientModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
